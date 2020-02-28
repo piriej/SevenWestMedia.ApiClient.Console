957c7fe78fbc073b32016460138d7a33e90a0518
Jeffrey Pirie <jeffrey.pirie@customerservice.nsw.gov.au>
2020-02-28 20:15:54 +1100

    Add project structure

---
+++ SevenWestMedia.ApiClient.Library.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SevenWestMedia.ApiClient.LibraryTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
